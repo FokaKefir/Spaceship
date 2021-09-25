ef6aa341b4bacfcff2811dbe221803a73ee14f93
FokaKefir <babosdavid8@gmail.com>
2021-09-25 23:42:11 +0300

    Adding invisible buttons.

--- app/src/main/java/com/fokakefir/spaceship/gui/fragment/CommanderFragment.java
+++ app/src/main/java/com/fokakefir/spaceship/gui/fragment/CommanderFragment.java
@@ -4,19 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.fokakefir.spaceship.R;
 import com.fokakefir.spaceship.gui.activity.MainActivity;
 
-public class CommanderFragment extends Fragment {
+public class CommanderFragment extends Fragment implements View.OnClickListener {
 
     // region 1. Decl and Init
 
     private View view;
 
     private MainActivity activity;
+
+    private Button btnCommanderTable;
 
     // endregion
 
@@ -30,7 +34,22 @@ public class CommanderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         this.view = inflater.inflate(R.layout.fragment_commander, container, false);
 
+        this.btnCommanderTable = this.view.findViewById(R.id.btn_commander_table);
+
+        this.btnCommanderTable.setOnClickListener(this);
+
         return this.view;
+    }
+
+    // endregion
+
+    // region 3. Button listener
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_commander_table) {
+            Toast.makeText(this.activity, "commander table", Toast.LENGTH_SHORT).show();
+        }
     }
 
     // endregion
--- app/src/main/java/com/fokakefir/spaceship/gui/fragment/TechnologyFragment.java
+++ app/src/main/java/com/fokakefir/spaceship/gui/fragment/TechnologyFragment.java
@@ -4,19 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.fokakefir.spaceship.R;
 import com.fokakefir.spaceship.gui.activity.MainActivity;
 
-public class TechnologyFragment extends Fragment {
+public class TechnologyFragment extends Fragment implements View.OnClickListener {
 
     // region 1. Decl and Init
 
     private View view;
 
     private MainActivity activity;
+
+    private Button btnDisplayTop;
+    private Button btnDisplayBottom;
 
     // endregion
 
@@ -30,7 +35,26 @@ public class TechnologyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         this.view = inflater.inflate(R.layout.fragment_technology, container, false);
 
+        this.btnDisplayTop = this.view.findViewById(R.id.btn_technology_display_top);
+        this.btnDisplayBottom = this.view.findViewById(R.id.btn_technology_display_bottom);
+
+        this.btnDisplayTop.setOnClickListener(this);
+        this.btnDisplayBottom.setOnClickListener(this);
+
         return this.view;
+    }
+
+    // endregion
+
+    // region 3. Button listener
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_technology_display_top) {
+            Toast.makeText(this.activity, "top display", Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.btn_technology_display_bottom) {
+            Toast.makeText(this.activity, "bottom display", Toast.LENGTH_SHORT).show();
+        }
     }
 
     // endregion
