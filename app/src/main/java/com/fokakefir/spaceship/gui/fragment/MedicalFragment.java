3b47e924045a9f3cc9489f045d6eeb2b9e9c698d
FoikaKefir <babosdavid8@gmail.com>
2021-09-27 19:13:18 +0300

    Adding Daily knowledge and some documents.

--- app/src/main/java/com/fokakefir/spaceship/gui/fragment/MedicalFragment.java
+++ app/src/main/java/com/fokakefir/spaceship/gui/fragment/MedicalFragment.java
@@ -15,7 +15,7 @@ import com.fokakefir.spaceship.R;
 import com.fokakefir.spaceship.gui.activity.MainActivity;
 import com.fokakefir.spaceship.gui.viewpager.ImageAdapter;
 
-public class MedicalFragment extends Fragment implements View.OnClickListener {
+public class MedicalFragment extends Fragment {
 
     // region 1. Decl and Init
 
@@ -25,9 +25,6 @@ public class MedicalFragment extends Fragment implements View.OnClickListener {
 
     private ViewPager viewPager;
     private ImageAdapter adapter;
-
-    private Button btnHeal;
-    private Button btnSport;
 
     // endregion
 
@@ -41,12 +38,6 @@ public class MedicalFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         this.view = inflater.inflate(R.layout.fragment_medical, container, false);
 
-        this.btnHeal = this.view.findViewById(R.id.btn_heal);
-        this.btnSport = this.view.findViewById(R.id.btn_sport);
-
-        this.btnHeal.setOnClickListener(this);
-        this.btnSport.setOnClickListener(this);
-
         this.viewPager = this.view.findViewById(R.id.view_pager);
         this.adapter = new ImageAdapter(getContext());
         this.viewPager.setAdapter(this.adapter);
@@ -56,16 +47,5 @@ public class MedicalFragment extends Fragment implements View.OnClickListener {
 
     // endregion
 
-    // region 3. Button listener
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btn_heal) {
-            Toast.makeText(activity, "Heal", Toast.LENGTH_SHORT).show();
-        } else if (v.getId() == R.id.btn_sport) {
-            Toast.makeText(activity, "Sport", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    // endregion
 }
--- app/src/main/java/com/fokakefir/spaceship/gui/fragment/commander/KnowledgeFragment.java
+++ app/src/main/java/com/fokakefir/spaceship/gui/fragment/commander/KnowledgeFragment.java
@@ -1,22 +1,42 @@
 package com.fokakefir.spaceship.gui.fragment.commander;
 
+import android.annotation.SuppressLint;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.fragment.app.Fragment;
 
 import com.fokakefir.spaceship.R;
 import com.fokakefir.spaceship.gui.activity.MainActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class KnowledgeFragment extends Fragment {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class KnowledgeFragment extends Fragment implements View.OnClickListener {
 
     // region 1. Decl and Init
 
     private View view;
 
     private MainActivity activity;
+
+    private WebView webViewDailyKnowledge;
+
+    private FloatingActionButton fabPrevious;
+    private FloatingActionButton fabNext;
+
+    private List<String> documentNames;
+    private String actualDocumentName;
 
     // endregion
 
@@ -26,11 +46,86 @@ public class KnowledgeFragment extends Fragment {
         this.activity = activity;
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         this.view = inflater.inflate(R.layout.fragment_commander_knowledge, container, false);
 
+        this.webViewDailyKnowledge = this.view.findViewById(R.id.vw_daily_knowledge);
+        this.fabPrevious = this.view.findViewById(R.id.fab_previous);
+        this.fabNext = this.view.findViewById(R.id.fab_next);
+
+        this.fabPrevious.setOnClickListener(this);
+        this.fabNext.setOnClickListener(this);
+
+        this.documentNames = new ArrayList<>();
+        loadDocumentNames();
+
+        this.actualDocumentName = this.documentNames.get(0);
+        this.webViewDailyKnowledge.getSettings().setJavaScriptEnabled(true);
+        this.webViewDailyKnowledge.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                injectCSS();
+                super.onPageFinished(view, url);
+            }
+        });
+        this.webViewDailyKnowledge.loadUrl("file:///android_asset/" + this.actualDocumentName);
+
         return this.view;
+    }
+
+    // endregion
+
+    // region 3. FloatingActionButton listener
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.fab_previous) {
+            int index = (this.documentNames.indexOf(this.actualDocumentName) - 1 + this.documentNames.size()) % this.documentNames.size();
+            this.actualDocumentName = this.documentNames.get(index);
+            this.webViewDailyKnowledge.loadUrl("file:///android_asset/" + this.actualDocumentName);
+        } else if (v.getId() == R.id.fab_next) {
+            int index = (this.documentNames.indexOf(this.actualDocumentName) + 1) % this.documentNames.size();
+            this.actualDocumentName = this.documentNames.get(index);
+            this.webViewDailyKnowledge.loadUrl("file:///android_asset/" + this.actualDocumentName);
+        }
+    }
+
+    // endregion
+
+    // region 4. Load documents and CSS file
+
+    private void loadDocumentNames() {
+        AssetManager assetManager = this.activity.getAssets();
+        try {
+            for (String file : assetManager.list("")) {
+                if (file.endsWith(".html"))
+                    this.documentNames.add(file);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Collections.shuffle(this.documentNames);
+    }
+
+    private void injectCSS() {
+        try {
+            InputStream inputStream = this.activity.getAssets().open("style.css");
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            inputStream.close();
+            String encoded = Base64.encodeToString(buffer, Base64.NO_WRAP);
+            this.webViewDailyKnowledge.loadUrl("javascript:(function() {" +
+                    "var parent = document.getElementsByTagName('head').item(0);" +
+                    "var style = document.createElement('style');" +
+                    "style.type = 'text/css';" +
+                    "style.innerHTML = window.atob('" + encoded + "');" +
+                    "parent.appendChild(style)" +
+                    "})()");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // endregion
