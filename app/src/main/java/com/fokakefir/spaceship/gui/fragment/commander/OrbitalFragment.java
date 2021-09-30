b5220aa2958bd6915c1792e884480d61a89f7154
FoikaKefir <babosdavid8@gmail.com>
2021-09-30 18:57:11 +0300

    Working on Orbital fragment.

--- app/src/main/java/com/fokakefir/spaceship/gui/fragment/commander/OrbitalFragment.java
+++ app/src/main/java/com/fokakefir/spaceship/gui/fragment/commander/OrbitalFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -18,6 +20,15 @@ public class OrbitalFragment extends Fragment {
 
     private MainActivity activity;
 
+    private TextView txtTraveledDistance;
+    private TextView txtDistance;
+    private TextView txtSpeed;
+    private TextView txtAcceleration;
+    private TextView txtTickRemaining;
+    private TextView txtTrackDeviation;
+
+    private ProgressBar progressDistance;
+
     // endregion
 
     // region 2. Lifecycle and constructor
@@ -30,7 +41,27 @@ public class OrbitalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         this.view = inflater.inflate(R.layout.fragment_commander_orbital, container, false);
 
+        this.txtTraveledDistance = this.view.findViewById(R.id.txt_orbital_traveled_distance);
+        this.txtDistance = this.view.findViewById(R.id.txt_orbital_distance);
+        this.txtSpeed = this.view.findViewById(R.id.txt_orbital_speed);
+        this.txtAcceleration = this.view.findViewById(R.id.txt_orbital_acceleration);
+        this.txtTickRemaining = this.view.findViewById(R.id.txt_orbital_tick_remaining);
+        this.txtTrackDeviation = this.view.findViewById(R.id.txt_orbital_track_deviation);
+        this.progressDistance = this.view.findViewById(R.id.progress_orbital_distance);
+
         return this.view;
+    }
+
+    // endregion
+
+    // region 3. Getters and Setters
+
+    public void setTraveledDistance(int distance) {
+        this.txtTraveledDistance.setText("Distance traveled: " + distance + " km");
+    }
+
+    public void setDistance(int distance) {
+        this.txtDistance.setText("Distance: " + distance + " km");
     }
 
     // endregion
